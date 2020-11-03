9a644a54ce44e12e2240b7bf0b858fc9a26f4fe2
Jawnathan <jonathan@jonathanschimpf.com>
2020-11-03 15:25:49 -0500

    navbar completely converted to JSX and working perfectly. hamburger drop down functions well and the padding is exactly where i intended, in it's slim format.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Navbar.css";
+import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-scroll";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function NavbarComp() {
@@ -8,30 +10,25 @@ function NavbarComp() {
 
         <>
 
-        <nav className="navbar navbar-expand-lg navbar-dark stickyNav">
-        <a className="navbar-brand" href="">JONATHAN SCHIMPF</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
-            aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
+        <Navbar className="navbar stickyNav"  fixed="top" variant="dark" expand="lg" bg="black">
+        <Navbar.Brand href="">JONATHAN SCHIMPF</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
 
-        <section className="collapse navbar-collapse d-lg-flex justify-content-end" id="navbarColor01">
-            <ul className="navbar-nav">
-                <li className="nav-item active">
-                    <a className="nav-link" href="">ABOUT <span className="sr-only">(ABOUT)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="">PORTFOLIO</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="">SKILLS</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="">CONTACT</a>
-                </li>
-            </ul>
-        </section>
-    </nav>
+        <Nav className="d-lg-flex ml-auto" id="navbarColor01">
+            
+                <Nav.Link className="nav-item active" href="">ABOUT <span className="sr-only">(ABOUT)</span>
+                </Nav.Link>
+                <Nav.Link className="nav-item" href="">PORTFOLIO
+                </Nav.Link>
+                <Nav.Link className="nav-item" href="">SKILLS
+                </Nav.Link>
+                <Nav.Link className="nav-item" href="">CONTACT
+                </Nav.Link>
+
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
 
         </>
 
@@ -40,3 +37,35 @@ function NavbarComp() {
 }
 
 export default NavbarComp;
+
+
+
+
+{/* <>
+
+<nav className="navbar navbar-expand-lg navbar-dark stickyNav">
+<a className="navbar-brand" href="">JONATHAN SCHIMPF</a>
+<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
+    aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+</button>
+
+<section className="collapse navbar-collapse d-lg-flex justify-content-end" id="navbarColor01">
+    <ul className="navbar-nav">
+        <li className="nav-item active">
+            <a className="nav-link" href="">ABOUT <span className="sr-only">(ABOUT)</span></a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="">PORTFOLIO</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="">SKILLS</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="">CONTACT</a>
+        </li>
+    </ul>
+</section>
+</nav>
+
+</> */}
