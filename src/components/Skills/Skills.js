7d4d1781085186d5f4ff512f6a63258d647f3883
Jawnathan <jonathan@jonathanschimpf.com>
2020-12-14 01:25:44 -0500

    updating tooltips to coincide with red skills section hover. tooltips give further description to hover effects to those curious about individual skills or languages.

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Skills.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Tooltip, OverlayTrigger } from "react-bootstrap";
 import { Link } from "react-scroll";
 
 
 
 function SkillsComp() {
     return (
-
+  
         <> 
+
         <div className="skillsBox">
 
        
@@ -37,116 +38,339 @@ function SkillsComp() {
             
             <Container className="nineSixtyWidth">
                 <Row className=" d-flex justify-content-center align-middle heightQuery">
+
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        HTML5
+                    </Tooltip> 
+                }   
+                    >
                     <div className="m-3">
                         <i className=" wow animate__animated animate__fadeIn animate__slower devicon-html5-plain-wordmark fa-4x skillsIcon skillsTransform" aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger>
 
+
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        CSS3
+                    </Tooltip> 
+                }   
+                    >
                     <div className="m-3">
                         <i className=" wow animate__animated animate__fadeIn animate__slower devicon-css3-plain-wordmark fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger>
 
+
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        JavaScript ES6+
+                    </Tooltip> 
+                }   
+                    >
                     <div className="m-3">
                         <i className=" wow animate__animated animate__fadeIn animate__slower devicon-javascript-plain fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger>
+                
 
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        jQuery Library
+                    </Tooltip> 
+                }   
+                    >
                     <div className="m-3">
                         <i className=" wow animate__animated animate__fadeIn animate__slower devicon-jquery-plain-wordmark fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
-                    
+                </OverlayTrigger>
+
+
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        React Library
+                    </Tooltip> 
+                }   
+                    >    
                     <div className="m-3">
                         <i className=" wow animate__animated animate__fadeIn animate__slower devicon-react-original-wordmark fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger>
 
+                
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        Bootstrap 
+                    </Tooltip> 
+                }   
+                    >  
                     <div className="m-3">
                         <i className="wow animate__animated animate__fadeIn animate__slower fab fa-bootstrap fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger>    
 
+
+
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        Node.js
+                    </Tooltip> 
+                }   
+                    >  
                     <div className="m-3">
                         <i className="wow animate__animated animate__fadeIn animate__slower devicon-nodejs-plain fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger> 
 
+                
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        Node Package Manager
+                    </Tooltip> 
+                }   
+                    >     
                     <div className="m-3">
                         <i className="wow animate__animated animate__fadeIn animate__slower fab fa-npm fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger>
 
+
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        Express.js
+                    </Tooltip> 
+                }   
+                    >  
                     <div className="m-3">
                         <i className="wow animate__animated animate__fadeIn animate__slower devicon-express-original fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger>
 
 
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        Visual Studio Code
+                    </Tooltip> 
+                }   
+                    >  
+                    <div className="m-3">
+                        <i className="wow animate__animated animate__fadeIn animate__slower devicon-visualstudio-plain fa-4x skillsIcon" aria-hidden="true"></i>
+                        <br />
+                    </div>
+                </OverlayTrigger>
+
+                
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        MongoDB
+                    </Tooltip> 
+                }   
+                    >  
                     <div className="m-3">
                         <i className="wow animate__animated animate__fadeIn animate__slower devicon-mongodb-plain-wordmark fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
-                    
+                </OverlayTrigger>
+
+                
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        MySQL
+                    </Tooltip> 
+                }   
+                    >      
                     <div className="m-3">
                         <i className="wow animate__animated animate__fadeIn animate__slower devicon-mysql-plain-wordmark fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger>
 
+
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        Sequelize
+                    </Tooltip> 
+                }   
+                    >  
                     <div className="m-3">
-                        <i className="wow animate__animated animate__fadeIn animate__slower devicon-sequelize-plain-wordmark fa-4x skillsIcon" aria-hidden="true"></i>
+                        <i className="wow animate__animated animate__fadeIn animate__slower devicon-sequelize-plain fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger>
 
+
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        Webpack Module Bundler
+                    </Tooltip> 
+                }   
+                    >  
                     <div className="m-3">
-                        <i className="wow animate__animated animate__fadeIn animate__slower devicon-visualstudio-plain-wordmark fa-4x skillsIcon" aria-hidden="true"></i>
+                        <i className="wow animate__animated animate__fadeIn animate__slower devicon-webpack-plain fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger>
 
+
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        Handlebars.js
+                    </Tooltip> 
+                }   
+                    >  
                     <div className="m-3">
-                        <i className="wow animate__animated animate__fadeIn animate__slower devicon-webpack-plain-wordmark fa-4x skillsIcon" aria-hidden="true"></i>
+                        <i className="wow animate__animated animate__fadeIn animate__slower devicon-handlebars-plain fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger>
 
-                    <div className="m-3">
-                        <i className="wow animate__animated animate__fadeIn animate__slower devicon-handlebars-plain-wordmark fa-4x skillsIcon" aria-hidden="true"></i>
-                        <br />
-                    </div>
 
-                    {/* <div className="m-3">
+                     {/* <div className="m-3">
                         <i className="wow animate__animated animate__fadeIn animate__slower devicon-babel-plain fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
-                    </div> */}
+                    </div>  */}
 
+
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        Adobe Photoshop
+                    </Tooltip> 
+                }   
+                    >  
                     <div className="m-3">
                         <i className="wow animate__animated animate__fadeIn animate__slower devicon-photoshop-plain fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
-                    
+                </OverlayTrigger>
+
+                
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        Adobe Premiere Pro
+                    </Tooltip> 
+                }   
+                    >      
                     <div className="m-3">
                         <i className="wow animate__animated animate__fadeIn animate__slower devicon-premierepro-plain  fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger>
 
+
+
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        Slack
+                    </Tooltip> 
+                }   
+                    >  
                     <div className="m-3">
                         <i className="wow animate__animated animate__fadeIn animate__slower devicon-slack-plain fa-4x skillsIcon " aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger>
 
+                
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        Git + GitHub
+                    </Tooltip> 
+                }   
+                    >      
                     <div className="m-3">
                         <i className="wow animate__animated animate__fadeIn animate__slower fab fa-git fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger>
 
+
+                
+                <OverlayTrigger
+                placement="top"
+                delay={{ show: 60, hide: 60 }}
+                overlay={
+                    <Tooltip>
+                        Heroku Cloud Platform
+                    </Tooltip> 
+                }   
+                    >      
                     <div className="m-3">
                         <i className="wow animate__animated animate__fadeIn animate__slower devicon-heroku-plain-wordmark fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
                     </div>
+                </OverlayTrigger>    
 
-                    <div className="m-3">
-                        <i className="wow animate__animated animate__fadeIn animate__slower devicon-github-plain-wordmark fa-4x skillsIcon" aria-hidden="true"></i>
+                    {/* <div className="m-3">
+                        <i className="wow animate__animated animate__fadeIn animate__slower devicon-github-plain fa-4x skillsIcon" aria-hidden="true"></i>
                         <br />
-                    </div>
+                    </div> */}
                     
                 </Row>
                 </Container>
