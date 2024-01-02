dcce85a22ca7a8ff81ebd7b450fe9b501218b663
Jawnathan <jonathan@jonathanschimpf.com>
2024-01-02 18:43:04 -0500

    cleaning up

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,19 +15,7 @@ function HeaderComp() {
             <Container className="centerHeader containerHeader" fluid>
                 <Container className="messageMargins">
                 <h1 className="display-4 blacked wow animate__animated animate__backInLeft responsiveH1Font">A DETAIL-ORIENTED DEVELOPER</h1>
-                <br />
                 <h2 className="display-4 greyed wow animate__animated animate__backInRight responsiveH2Font">WHO LOVES TO CONTINUE LEARNING</h2>
-                <br />
-                <Link
-                    href=""
-                    to="about"
-                    spy={true}
-                    smooth={true}
-                    // offset={-50}
-                    duration={800}
-                    delay={150}
-                >
-                </Link>
                 </Container>
             </Container>
           </span> 
