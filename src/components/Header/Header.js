6482b9f7c5b8f458b49dca8940e3ddff5f8130b8
Jawnathan <jonathan@jonathanschimpf.com>
2024-05-05 19:01:17 -0400

    wow.js CDN change because the offering is deprecated and the old link flat out stopped working after 4 years of being fine lol

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,25 @@
-
 import React from "react";
 import "./Header.css";
 import { Container } from "react-bootstrap";
-import { Link } from 'react-scroll'
-
-
+import { Link } from "react-scroll";
 
 function HeaderComp() {
-
-    return (
-
-        <>
-          <span id="header">
-            <Container className="centerHeader containerHeader" fluid>
-                <Container className="messageMargins">
-                <h1 className="display-4 blacked wow animate__animated animate__backInLeft responsiveH1Font">A DETAIL-ORIENTED DEVELOPER</h1>
-                <h2 className="display-4 greyed wow animate__animated animate__backInRight responsiveH2Font">WHO LOVES TO CONTINUE LEARNING</h2>
-                </Container>
-            </Container>
-          </span> 
-        </>
-
-
-    );
+  return (
+    <>
+      <span id="header">
+        <Container className="centerHeader containerHeader" fluid>
+          <Container className="messageMargins">
+            <h1 className="display-4 blacked wow animate__animated animate__backInLeft responsiveH1Font">
+              A DETAIL-ORIENTED DEVELOPER
+            </h1>
+            <h2 className="display-4 greyed wow animate__animated animate__backInRight responsiveH2Font">
+              WHO LOVES TO CONTINUE LEARNING
+            </h2>
+          </Container>
+        </Container>
+      </span>
+    </>
+  );
 }
 
 export default HeaderComp;
-
-
-
